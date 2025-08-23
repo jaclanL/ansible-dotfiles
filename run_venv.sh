279b0ad732682b7921f04e5caa1eaf17efd9ae6d
jaclanL <44745949+jaclanL@users.noreply.github.com>
2025-08-23 10:01:18 +0200

    Add pre-commit
    Pre-commit to perform simple checks and linting

---
+++ run_venv.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set +x
+
+function get_script_dir() {
+  cd "$(dirname "$0")" >/dev/null 2>&1 && pwd
+}
+
+if [[ $0 == "./"* ]]; then
+  echo "File needs to be sourced with 'source run_venv.sh'"
+  exit 1
+fi
+
+repo_root=$(git -C "$(get_script_dir)" rev-parse --show-toplevel)
+
+echo "Starting venv... @ ${repo_root}/.venv"
+python3 -m venv "${repo_root}/.venv"
+# shellcheck source=/dev/null
+source "${repo_root}/.venv/bin/activate"
+
+if [[ -f "${repo_root}/requirements.txt" ]]; then
+  python3 -m pip install \
+    --disable-pip-version-check \
+    -r "${repo_root}/requirements.txt" > /dev/null
+fi
+
+if [ ! -f "${repo_root}/.git/hooks/pre-commit" ]; then
+    pre-commit install
+fi
+
+echo "To stop venv: 'deactivate'"
