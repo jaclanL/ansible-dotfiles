1f68f30591508d2156e2aa8a3903508c20ac51c4
jaclanL <44745949+jaclanL@users.noreply.github.com>
2025-09-01 21:03:34 +0200

    Simplify the path logic in run_venv.sh
    Also, don't run pre-commit install when in CI.

--- run_venv.sh
+++ run_venv.sh
@@ -1,26 +1,11 @@
 #!/usr/bin/env bash
 
-function get_script_dir() {
-  # Globals:
-  #   $0: path to the script to invoke this script
-  # Outputs:
-  #   script_dir
-  local source="${BASH_SOURCE[0]}"
-  while [ -L "${source}" ]; do
-      local dir
-      dir="$(cd -P "$(dirname -- "${source}")" >/dev/null 2>&1 && pwd)"
-      source="$(readlink -- "${source}")"
-      [[ "${source}" != /* ]] && source="${dir}/${source}"
-  done
-  cd -P "$(dirname -- "${source}")" >/dev/null 2>&1 && pwd
-}
-
 if [[ $0 == "./"* ]]; then
   echo "File needs to be sourced with 'source run_venv.sh'"
   exit 1
 fi
 
-repo_root=$(get_script_dir)
+repo_root="$(dirname "$(realpath "$0")")"
 
 echo "Starting venv... @ ${repo_root}/.venv"
 python3 -m venv "${repo_root}/.venv"
@@ -33,8 +18,9 @@ if [[ -f "${repo_root}/requirements.txt" ]]; then
     -r "${repo_root}/requirements.txt" > /dev/null
 fi
 
-if [ ! -f "${repo_root}/.git/hooks/pre-commit" ]; then
-    pre-commit install
+# Don't install pre-commit if in GitHub Actions or if already exist
+if [ -z "${GITHUB_ACTIONS}" ] && [ ! -f "${repo_root}/.git/hooks/pre-commit" ]; then
+  pre-commit install
 fi
 
 echo "To stop venv: 'deactivate'"
